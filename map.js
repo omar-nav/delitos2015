2acf935e8fd115c41a793bf2d82c91a81da45191
Omar Cesar <omar-nav@users.noreply.github.com>
2018-07-26 18:26:11 -0500

    agregar leyenda

--- map.js
+++ map.js
@@ -28,21 +28,65 @@ function styleSecuestros(feature) {
     }
 }
 //
-function geojsonPopup(feature, layer){
-     if(feature.properties.NOMGEO != null){
-        layer.bindPopup('Estado:   ' + feature.properties.NOMGEO + '<br>Secuestros en 2015:   '+ feature.properties._Total);
-    }
-}
+
 // function label(feature, layer){
 //     if(feature.properties.refactor_4 != null){
 //         layer.bindTooltip(feature.properties.refactor_4 + ' per 100,000',{direction: 'center'},{permanent: true},{opacity: .1});
 //         layer.bindPopup('Country:   ' + feature.properties.SOVEREIGNT + '<br>Total Homicides:   '+ feature.properties.refactor_3 + '<br>Homicide Rate:   '+ feature.properties.refactor_4 + ' per 100,000')
 //     }
 // }
-L.geoJSON([secuestros2015], {
+var Secuestros2015Layer = L.geoJSON([secuestros2015], {
     style: styleSecuestros,
      onEachFeature: geojsonPopup,
     pointToLayer: function (feature, latlng){
         return L.marker(latlng);
     }
-}).addTo(mymap);
+});
+
+function geojsonPopup(feature, layer){
+    if(feature.properties.NOMGEO != null){
+        layer.bindPopup('Estado:   ' + feature.properties.NOMGEO + '<br>Secuestros en 2015:   '+ feature.properties._Total);
+    }
+}
+
+Secuestros2015Layer.addTo(mymap);
+var featureLayers = {
+    "2015 Secuestros": Secuestros2015Layer
+//    "2017 January Zoning": Zoning2017Layer
+};
+var geojson = L.control.layers(featureLayers).addTo(mymap);
+
+// LEGEND STARTS HERE
+var SecuestrosParcelLegend = L.control({position: 'bottomright'});
+
+SecuestrosParcelLegend.onAdd = function (mymap) {
+    var div = L.DomUtil.create('div', 'info legend'),
+        grades = [0, 5, 11, 24, 80, 162],
+        labels = ['Suma de Secuestros 2015 <br> por estado'],
+        from, to;
+    for (var i = 0; i < grades.length; i++) {
+        from = grades[i];
+        to = grades[i + 1];
+        labels.push(
+            '<i style="background' + choroplethize(from + 1) + '"></i> ' +
+            from + (to ? ' - ' + to : ' +'));
+    }
+    div.innerHTML = labels.join('<br>');
+    return div;
+};
+
+SecuestrosParcelLegend.addTo(mymap);
+currentLegend = SecuestrosParcelLegend;
+mymap.on('overlayadd', function (eventLayer) {
+    if  (eventLayer.name === '2015 Secuestros') {
+        mymap.removeControl(currentLegend);
+        currentLegend = SecuestrosParcelLegend;
+        SecuestrosParcelLegend.addTo(mymap);
+    }
+    // else if  (eventLayer.name === '2000 Census Population') {
+    //     map.removeControl(currentLegend);
+    //     currentLegend = Population2000Legend;
+    //     Population2000Legend.addTo(map);
+    // }
+});
+// LEGEND ENDS HERE
